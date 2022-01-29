37d5b247a96f7a4b6f4d6876cb8e5652846ff387
Leszek Wydro <leszek.wydro@gmail.com>
2022-01-29 18:01:10 +0100

    Add  what/if to script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
+
 if (randomNumber == '1') {
   computerMove = 'kamień';
 } else {
